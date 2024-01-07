5e4084f3712a26be50cb00c8765f3588ad6d9ea8
unknown <MituSarker>
2024-01-07 08:48:08 -0800

    add new components

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,31 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+
+
+import Main from "../Layout/Main";
+import Home from "../Layout/Pages/Home/Home/Home";
+import Menu from "../Layout/Pages/Menu/Menu/Menu";
+import Order from "../Layout/Pages/Order/Order/Order";
+
+
+  export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+            path: '/',
+            element:<Home></Home>
+        },
+        {
+          path:"menu",
+          element:<Menu></Menu>
+        },
+       {
+        path: 'order/:category',
+        element:<Order></Order>
+       }
+      ]
+    },
+  ]);
